3e3e6bf6058eafff1e8a47087906279f13bca8b4
0xjoshva <joshuasteed123@gmail.com>
2022-10-26 22:36:04 +0200

    animation thank you

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -13,35 +13,48 @@
         <p>420</p>
       </div>
     </div>
-    <form action="post">
+    <form @submit.prevent="onSubmit">
+      <div class="inputs" v-show="submit === false">
       <label for="name">
         CARDHOLDER NAME <br />
-        <input name="name" type="text" placeholder="e.g Quandale Dingle" />
+        <input name="name" type="text" placeholder="e.g Quandale Dingle" required/>
       </label>
       <label for="number">
         CARD NUMBER <br />
-        <input name="number" type="text" placeholder="e.g 0238 2738 2838 2821" />
+        <input name="number" type="text" placeholder="e.g 0238 2738 2838 2821" required/>
       </label>
       <div id="details">
         <label for="date">
           EXP. DATE (MM/YY) <br />
           <div id="mmyy">
-            <input name="date" type="number" placeholder="MM" min="1" max="12"/>
-            <input type="number" placeholder="YY" min="22" max="29"/>
+            <input name="date" type="number" placeholder="MM" min="1" max="12" required/>
+            <input type="number" placeholder="YY" min="22" max="32" required/>
           </div>
         </label>
         <label for="cvc">
           CVC <br />
-          <input type="text" placeholder="e.g 123"  />
+          <input type="text" placeholder="e.g 123"  required/>
         </label>
       </div>
-      <button>Confirm</button>
+      </div>
+      <div v-show="submit === true" id="thanks">
+        <img src="../assets/icon-complete.svg" alt="">
+      <H1>THANK YOU!</H1>
+      <p>We've added your card details</p>
+      </div>
+      <button @click="submit = true">Confirm</button>
     </form>
   </section>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      submit: false
+    }
+  }
+};
 </script>
 <style scoped>
 section {
@@ -88,7 +101,6 @@ form {
   margin-top: 4.5rem;
   background: transparent;
   border-radius: 12px;
-  padding-left: 10px;
 }
 .card-details{
   display: flex;
@@ -111,6 +123,7 @@ form {
     font-size: 1rem;
     transform: translate(360px, 110px);
     letter-spacing: 3px;
+    width: fit-content;
 
 }
 .card{
@@ -144,6 +157,11 @@ form{
   width: 25rem;
   row-gap: 1rem;
 }
+.inputs{
+  row-gap: 1.1rem;
+  display: flex;
+  flex-direction: column;
+}
 label{
   font-size: 1rem;
   font-weight: 500;
@@ -165,8 +183,7 @@ input:focus{
   outline: none;
   border-width: 1px;
   border-style: solid;
-  border-image: linear-gradient(to right bottom, #260B3C, #a053df);
-  border-image-slice: 1;
+  border-color: hsl(278, 94%, 30%);
 }
 ::placeholder{
   color: var(--lightgreyviolet);
@@ -177,6 +194,14 @@ form button{
   color: var(--white);
   font-size: 1.2rem;
   padding: .8rem;
+  transition: 0.2s all ease-in-out;
+  border: none;
+}
+form button:active{
+  transform: translateY(-10px);
+}
+form button:hover{
+  background: rgb(92, 0, 128);
 }
 #mmyy{
   display: flex;
@@ -191,5 +216,36 @@ form button{
   column-gap: 2rem;
   padding-bottom: 1.6rem;
 }
-
+#thanks{
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  margin-bottom: 4rem;
+  animation: fade-in 0.2s ease-in-out;
+}
+#thanks img{
+  width: 5rem;
+  margin: 0 auto;
+  height: auto;
+  margin-bottom: 2rem;
+}
+#thanks h1{
+  font-size: 1.8rem;
+  font-weight: 500;
+  letter-spacing: 3px;
+  color: var(--verydarkviolet);
+  padding-bottom: .5rem;
+}
+#thanks p{
+  color: var(--darkgreyviolet);
+  font-weight: 400;
+}
+@keyframes fade-in{
+  0%{
+    opacity: 0.1;
+  }
+  100%{
+    opacity: 1;
+  }
+}
 </style>
